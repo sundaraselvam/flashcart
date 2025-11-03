81c21f5597de6e5ea9648e7d45259c70c9c0f74b
sundaraselvam <vickiesundar2000@gmail.com>
2025-11-03 14:38:58 +0530

    added products to display and designed cards+backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await axios("https://fakestoreapi.com/products");
+      setItems(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="grid">
+      {items.map((ele, index) => {
+        return (
+          <div key={index} className="card">
+            <h2 className="card-title">{ele.title}</h2>
+            <img className="card-image" src={ele.image} alt={ele.title} />
+            <p className="card-desc">{ele.description}</p>
+            <div className="card-footer">
+              <span className="card-price">${ele.price}</span>
+              <div className="buttons">
+                <button>Add to cart</button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
